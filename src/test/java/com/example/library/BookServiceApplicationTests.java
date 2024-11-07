82bb2f5a2dede18cb2955a7af266743def1b9ed1
Victor Torres <vtorresm86@gmail.com>
2024-11-07 16:23:08 -0500

    Added unit tests and modified some imports to avoid the * wildcards

--- src/test/java/com/example/library/BookServiceApplicationTests.java
+++ src/test/java/com/example/library/BookServiceApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.library;
 
+import com.example.library.mapper.BookMapper;
+import com.example.library.repository.BookRepository;
+import com.example.library.service.BookService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BookServiceApplicationTests {
 
+	@Autowired
+	private BookService bookService;
+
+	@Autowired
+	private BookMapper bookMapper;
+
+	@Autowired
+	private BookRepository bookRepository;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(bookService, "BookService should be loaded in the application context");
+		assertNotNull(bookMapper, "BookMapper should be loaded in the application context");
+		assertNotNull(bookRepository, "BookRepository should be loaded in the application context");
 	}
 
 }
